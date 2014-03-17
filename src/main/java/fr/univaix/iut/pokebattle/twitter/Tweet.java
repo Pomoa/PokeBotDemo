9c2828e90ed09bf4c46b3d3c503a20e95a22fb31
Stilldead <pierreburgain@gmail.com>
2014-03-17 11:13:02 +0100

    Ajout de hashTag dans PokeBot**

--- src/main/java/fr/univaix/iut/pokebattle/twitter/Tweet.java
+++ src/main/java/fr/univaix/iut/pokebattle/twitter/Tweet.java
@@ -3,6 +3,7 @@ package fr.univaix.iut.pokebattle.twitter;
 public class Tweet {
     private String text;
     private String screenName;
+    private String[] hashTag;
 
     public Tweet(String text) {
         this.text = text;
@@ -11,6 +12,20 @@ public class Tweet {
     public Tweet(String screenName, String text) {
         this.screenName = screenName;
         this.text = text;
+        
+        for (int i = 0, p=0 ; i < text.length() -1 ; i++, p++) {// parcours le tweet
+        	if (text.charAt(i) == '#') 							// s'arrete si il croise un hashtag
+        	{
+        		i++;
+        		Character car = text.charAt(i);
+        		for ( ; Character.isLetterOrDigit(car) ; )		 // vérifie si le caractere met fin au hashtag
+        		{
+        			hashTag[p] += car; 
+        			car = text.charAt(++i);	
+        		}  
+        	}        	
+        	
+        }
     }
 
     public String getScreenName() {
@@ -20,4 +35,8 @@ public class Tweet {
     public String getText() {
         return text;
     }
+
+	public String getHashTag(int i) {
+		return hashTag[i];
+	}
 }

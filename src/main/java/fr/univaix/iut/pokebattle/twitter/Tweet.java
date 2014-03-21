8eb5ebd163a457773dbb9d7cc9bd8532216a6026
Tristan <tristan.ughetti@pomoa.fr>
2014-03-21 17:22:18 +0100

    Tweet HashTag reduction debug

--- src/main/java/fr/univaix/iut/pokebattle/twitter/Tweet.java
+++ src/main/java/fr/univaix/iut/pokebattle/twitter/Tweet.java
@@ -1,31 +1,34 @@
 package fr.univaix.iut.pokebattle.twitter;
 
+import java.util.ArrayList;
+
 public class Tweet {
     private String text;
     private String screenName;
-    private String[] hashTag;
+    private ArrayList<String> hashTag = new ArrayList<String>();
 
     public Tweet(String text) {
         this.text = text;
     }
 
     public Tweet(String screenName, String text) {
-        this.screenName = screenName;
+    	this.screenName = screenName;
         this.text = text;
         
-        /*for (int i = 0, p = 0 ; i < text.length() -1 ; i++, p++) {// parcours le tweet
-        	if (text.charAt(i) == '#') 							// s'arrete si il croise un hashtag
+        for (int i = 0; i < text.length() ; i++) {							// parcours le tweet
+        	if (text.charAt(i) == '#') 										// s'arrete si il croise un hashtag
         	{
-        		i++;
-        		char car = text.charAt(i);
-        		for ( ; Character.isLetterOrDigit(car) ; )		 // vérifie si le caractere met fin au hashtag
+        		String hashtag = new String("");
+        		for ( ; Character.isLetterOrDigit(text.charAt(++i)) ; )		// vérifie si le caractere met fin au hashtag
         		{
-        			hashTag[p] += car; 
-        			car = text.charAt(++i);
-        		}  
+        			hashtag += text.charAt(i);
+        		}
+        		hashtag += text.charAt(i);
+        		
+        		hashTag.add(hashtag);
         	}        	
         	
-        }*/
+        }
     }
 
     public String getScreenName() {
@@ -37,6 +40,6 @@ public class Tweet {
     }
 
 	public String getHashTag(int i) {
-		return hashTag[i];
+		return hashTag.get(i);
 	}
 }

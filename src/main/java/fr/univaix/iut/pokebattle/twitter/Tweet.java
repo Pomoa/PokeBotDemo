b820ed47efcbe6af7b8e0944810b7266ce770498
Tristan <tristan.ughetti@pomoa.fr>
2014-03-24 09:17:08 +0100

    hashtag++

--- src/main/java/fr/univaix/iut/pokebattle/twitter/Tweet.java
+++ src/main/java/fr/univaix/iut/pokebattle/twitter/Tweet.java
@@ -19,15 +19,20 @@ public class Tweet {
         	if (text.charAt(i) == '#') 										// s'arrete si il croise un hashtag
         	{
         		String hashtag = new String("");
-        		for ( ; Character.isLetterOrDigit(text.charAt(++i)) ; )		// vérifie si le caractere met fin au hashtag
+        		System.out.print(hashtag);
+        		System.out.println(i);
+        		for ( ; Character.isLetterOrDigit(text.charAt(++i)) && i < text.length(); )		// vérifie si le caractere met fin au hashtag
         		{
         			hashtag += text.charAt(i);
+            		System.out.print(hashtag);
+            		System.out.println(i);
         		}
         		hashtag += text.charAt(i);
+        		System.out.print(hashtag);
+        		System.out.println(i);
         		
         		hashTag.add(hashtag);
-        	}        	
-        	
+        	} 
         }
     }
 
--- src/test/java/fr/univaix/iut/pokebattle/twitter/TestTweet.java
+++ src/test/java/fr/univaix/iut/pokebattle/twitter/TestTweet.java
@@ -5,16 +5,23 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TestTweet {
+//    @Test
+//    public void testCreateTweet() {
+//        Tweet tweet = new Tweet("nedseb", "coucou !");
+//        assertEquals("nedseb", tweet.getScreenName());
+//        assertEquals("coucou !", tweet.getText());
+//
+//        Tweet tweet2 = new Tweet("coucou !");
+//        assertEquals(null, tweet2.getScreenName());
+//        assertEquals("coucou !", tweet2.getText());
+//
+//    }
+    
     @Test
-    public void testCreateTweet() {
-        Tweet tweet = new Tweet("nedseb", "coucou !");
-        assertEquals("nedseb", tweet.getScreenName());
-        assertEquals("coucou !", tweet.getText());
-
-        Tweet tweet2 = new Tweet("coucou !");
-        assertEquals(null, tweet2.getScreenName());
-        assertEquals("coucou !", tweet2.getText());
-
+    public void testHashtag(){
+    	Tweet tweet = new Tweet("nedseb", "Coucou ! #gentil #bonjour");
+    	assertEquals("nedseb", tweet.getScreenName());
+    	assertEquals("gentil", tweet.getHashTag(0));
+    	assertEquals("bonjour", tweet.getHashTag(1));
     }
-
 }

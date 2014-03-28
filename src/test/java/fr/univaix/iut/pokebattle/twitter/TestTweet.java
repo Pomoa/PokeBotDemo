1bd5b5b8857074e9ac7ad56040e083c470dee9c2
Pomoa <tristan.ughetti@pomoa.fr>
2014-03-28 14:38:41 +0100

    Suppression de la mise en comentaire

--- src/test/java/fr/univaix/iut/pokebattle/twitter/TestTweet.java
+++ src/test/java/fr/univaix/iut/pokebattle/twitter/TestTweet.java
@@ -5,17 +5,17 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TestTweet {
-//    @Test
-//    public void testCreateTweet() {
-//        Tweet tweet = new Tweet("nedseb", "coucou !");
-//        assertEquals("nedseb", tweet.getScreenName());
-//        assertEquals("coucou !", tweet.getText());
-//
-//        Tweet tweet2 = new Tweet("coucou !");
-//        assertEquals(null, tweet2.getScreenName());
-//        assertEquals("coucou !", tweet2.getText());
-//
-//    }
+    @Test
+    public void testCreateTweet() {
+        Tweet tweet = new Tweet("nedseb", "coucou !");
+        assertEquals("nedseb", tweet.getScreenName());
+        assertEquals("coucou !", tweet.getText());
+
+        Tweet tweet2 = new Tweet("coucou !");
+        assertEquals(null, tweet2.getScreenName());
+        assertEquals("coucou !", tweet2.getText());
+
+    }
     
     @Test
     public void testHashtag(){
